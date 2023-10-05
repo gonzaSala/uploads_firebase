b43e485305fda3af08c9b5144f89d4d69736e89a
gonzaSala <gonzasalamendi@gmail.com>
2023-10-04 21:11:41 -0300

    agregamos seleccion img desde galeria

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,18 +1,47 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class HomePage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:uploads_firebase/services/select_img.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({
     super.key,
   });
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  File? imageToUpload;
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Material App Bar'),
+        title: const Text('Img'),
       ),
-      body: const Center(
-        child: Text('Hello World'),
+      body: Column(
+        children: [
+          imageToUpload != null
+              ? Image.file(imageToUpload!)
+              : Container(
+                  margin: const EdgeInsets.all(10),
+                  height: 200,
+                  width: double.infinity,
+                  color: Colors.redAccent,
+                ),
+          ElevatedButton(
+              onPressed: () async {
+                final image = await getImg();
+                setState(() {
+                  imageToUpload = File(image!.path);
+                });
+              },
+              child: const Text('Seleccionar imagen')),
+          ElevatedButton(onPressed: () {}, child: const Text('Subir imagen')),
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'package:uploads_firebase/home_page.dart';
 
 void main() => runApp(const MyApp());
